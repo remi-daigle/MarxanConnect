0bc8cab94c2e5a5e5558d36e27f9f6da3b721280
Remi Daigle <remi.daigle@dal.ca>
2017-09-28 12:34:29 -0300

    build mac version

--- setup.py
+++ setup.py
@@ -3,42 +3,59 @@ import sys
 import os
 from cx_Freeze import setup, Executable
 
-# editing out deprecated functions in gui.py
-# Read in the file
-with open('gui.py', 'r', encoding="utf8") as file :
-  filedata = file.read()
+if os.name=='nt':
+    # editing out deprecated functions in gui.py
+    # Read in the file
+    with open('gui.py', 'r', encoding="utf8") as file :
+      filedata = file.read()
 
-# Replace the target string
-filedata = filedata.replace('SetToolTipString', 'SetToolTip')
+    # Replace the target string
+    filedata = filedata.replace('SetToolTipString', 'SetToolTip')
 
-# Write the file out again
-with open('gui.py', 'w', encoding="utf8") as file:
-  file.write(filedata)
+    # Write the file out again
+    with open('gui.py', 'w', encoding="utf8") as file:
+      file.write(filedata)
 
-# prep for cx_Freeze
-os.environ['TCL_LIBRARY'] = os.path.join(os.environ['LOCALAPPDATA'],'Programs','Python','Python35','tcl','tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(os.environ['LOCALAPPDATA'],'Programs','Python','Python35','tcl','tk8.6')
+    # prep for cx_Freeze
+    os.environ['TCL_LIBRARY'] = os.path.join(os.environ['LOCALAPPDATA'],'Programs','Python','Python35','tcl','tcl8.6')
+    os.environ['TK_LIBRARY'] = os.path.join(os.environ['LOCALAPPDATA'],'Programs','Python','Python35','tcl','tk8.6')
 
-# define additional modules (those not automatically found)
-build_exe_options = {'includes': ['numpy.core._methods', 'numpy.lib.format','matplotlib.backends.backend_qt5agg',
-                                  'matplotlib.backends.backend_tkagg','tkinter','tkinter.filedialog','igraph',
-                                  'igraph.vendor.texttable'],
-                     'include_files': ['data/','gui.py','glossary.html','tutorial.html','contributing.html',
-                                       os.path.join(os.environ['LOCALAPPDATA'],
-                                                    'Programs','Python','Python35','DLLs','tcl86t.dll'),
-                                       os.path.join(os.environ['LOCALAPPDATA'],
-                                                    'Programs','Python','Python35','DLLs','tk86t.dll'),
-                                       os.path.join(sys.path[0],'icon_bundle.ico')],
-                     'namespace_packages': ['mpl_toolkits'] }
+    # define additional modules (those not automatically found)
+    build_exe_options = {'includes': ['numpy.core._methods', 'numpy.lib.format','matplotlib.backends.backend_qt5agg',
+                                      'matplotlib.backends.backend_tkagg','tkinter','tkinter.filedialog','igraph',
+                                      'igraph.vendor.texttable'],
+                         'include_files': ['data/','gui.py','glossary.html','tutorial.html','contributing.html',
+                                           os.path.join(os.environ['LOCALAPPDATA'],
+                                                        'Programs','Python','Python35','DLLs','tcl86t.dll'),
+                                           os.path.join(os.environ['LOCALAPPDATA'],
+                                                        'Programs','Python','Python35','DLLs','tk86t.dll'),
+                                           os.path.join(sys.path[0],'icon_bundle.ico')],
+                         'namespace_packages': ['mpl_toolkits'] }
 
-base = None
-if sys.platform == 'win32':
-    base = 'Win32GUI'
+    base = None
+    if sys.platform == 'win32':
+        base = 'Win32GUI'
 
-setup(name = 'MarxanConnectGUI' ,
-      version = '0.1' ,
-      description = '' ,
-      options = {'build_exe': build_exe_options},
-      executables = [Executable('MarxanConnectGUI.py', base=base, icon=os.path.join(sys.path[0],'icon_bundle.ico'))])
+    setup(name = 'MarxanConnectGUI' ,
+          version = '0.1' ,
+          description = '' ,
+          options = {'build_exe': build_exe_options},
+          executables = [Executable('MarxanConnectGUI.py', base=base, icon=os.path.join(sys.path[0],'icon_bundle.ico'))])
+else:
+    # define additional modules (those not automatically found)
+    build_exe_options = {'includes': ['numpy.core._methods','packaging.version','packaging.specifiers',
+                                      'packaging.requirements','igraph.vendor.texttable'],
+                         'include_files': ['data/', 'gui.py', 'glossary.html', 'tutorial.html', 'contributing.html',
+                                           os.path.join(sys.path[0], 'icon_mac.icns')],
+                         }
+
+    mac_options = {'iconfile': os.path.join(sys.path[0], 'icon_mac.icns')}
+
+    setup(name='MarxanConnectGUI',
+          version='0.1',
+          description='',
+          options={'build_exe': build_exe_options,
+                   'bdist_mac': mac_options},
+          executables=[Executable('MarxanConnectGUI.py')])
 
 
