b3b57118e05ecca2bffb949a7e56f82e9ac981c1
Remi Daigle <remi.daigle@dal.ca>
2018-08-07 11:21:13 -0300

    beetweenness on map for pub

--- docs/maps_for_pubs/map_for_pub.R
+++ docs/maps_for_pubs/map_for_pub.R
@@ -1,3 +1,68 @@
+BIORE <- read.csv("maps_for_pubs/input/spec.dat") %>% 
+    select(name) %>% 
+    filter(grepl("BIORE_",name)) %>% 
+    mutate(name=gsub("BIORE_","",name)) %>% 
+    unlist() %>% 
+    as.numeric()
+
+proj <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
+
+
+bioregions <- st_read("maps_for_pubs/bioregion_short.shp",stringsAsFactors = FALSE) %>% 
+    filter(BIOREGI %in% BIORE) %>% 
+    mutate(BIOREGI=paste0("BIORE_",BIOREGI)) %>% 
+    st_transform(proj)
+
+pu <- read.csv("maps_for_pubs/output/pu_no_connect.csv") %>%
+    mutate(geometry=st_as_sfc(geometry,"+proj=longlat +datum=WGS84"),
+           best_solution = as.logical(best_solution)) %>%
+    st_as_sf()%>% 
+    st_transform(proj) %>% 
+    select(pu_id)
+
+# st_write(bioregions,"maps_for_pubs/bioregion_filtered.shp")
+
+intersection <- st_intersection(pu,bioregions)
+intersection$row <- 1:nrow(intersection)
+distances <- st_distance(intersection) %>% 
+    data.frame(check.names = FALSE) %>% 
+    mutate(row=row.names(.)) %>% 
+    gather(key="column",value="value",-row)
+
+distances$row <- as.numeric(distances$row)
+distances$column <- as.numeric(distances$column)
+# con_list <- distances
+
+con_list <- distances %>%
+    left_join(data.frame(intersection),by=c("row"="row")) %>% 
+    mutate(id1=as.numeric(pu_id),
+           from_BIO=BIOREGI) %>%
+    select(row,column,id1,from_BIO,value) %>% 
+    left_join(data.frame(intersection),by=c("column"="row")) %>% 
+    mutate(id2=as.numeric(pu_id),
+           to_BIO=BIOREGI) %>%
+    select(id1,from_BIO,id2,to_BIO,value) %>% 
+    filter(from_BIO==to_BIO,
+           as.numeric(value)>0) %>% 
+    mutate(habitat=to_BIO) %>% 
+    group_by(habitat,id1,id2) %>% 
+    summarize(value=1/mean(as.numeric(value))^2) %>% 
+    group_by(habitat,id1) %>% 
+    mutate(value=as.numeric(value)/sum(as.numeric(value)))
+
+write.csv(con_list,"maps_for_pubs/IsolationByDistance.csv",quote=FALSE,row.names = FALSE)
+
+con_list_mean <- con_list %>% 
+    group_by(id1,id2) %>% 
+    summarize(value=mean(value)) %>% 
+    mutate(habitat="mean") %>% 
+    as.data.frame() %>% 
+    complete(habitat,id1,id2,fill=list(value = 0)) %>% 
+    as.data.frame()
+
+write.csv(con_list_mean,"maps_for_pubs/IsolationByDistance_mean.csv",quote=FALSE,row.names = FALSE)
+
+
 # CF_landscape maps for publication
 library(sf)
 library(raster)
@@ -9,9 +74,9 @@ library(tidyverse)
 proj <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
 proj_rotated <- "+proj=omerc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs +alpha=-73.00000"
 
-spec <- read.csv("tutorial/CF_landscape/input/spec.dat")
-puvspr <- read.csv("tutorial/CF_landscape/input/puvspr2.dat")
-pu <- read.csv("tutorial/CF_landscape/input/pu.dat")
+spec <- read.csv("maps_for_pubs/input/spec.dat")
+puvspr <- read.csv("maps_for_pubs/input/puvspr2.dat")
+pu <- read.csv("maps_for_pubs/input/pu.dat")
 
 puvspr_wide <- puvspr %>%
     left_join(select(spec,"id","name"),
@@ -20,7 +85,7 @@ puvspr_wide <- puvspr %>%
     spread(key="name",value="amount")
 
 # planning units with output
-output <- read.csv("tutorial/CF_landscape/output/pu_no_connect.csv") %>%
+output <- read.csv("maps_for_pubs/output/pu_no_connect.csv") %>%
     mutate(geometry=st_as_sfc(geometry,"+proj=longlat +datum=WGS84"),
            best_solution = as.logical(best_solution)) %>%
     st_as_sf() %>%
@@ -30,14 +95,14 @@ output <- read.csv("tutorial/CF_landscape/output/pu_no_connect.csv") %>%
 output_rotated <- st_transform(output,proj_rotated)
 
 
-output_connect <- read.csv("tutorial/CF_landscape/output/pu_connect.csv") %>%
+output_connect <- read.csv("maps_for_pubs/output/pu_connect.csv") %>%
     mutate(geometry=st_as_sfc(geometry,"+proj=longlat +datum=WGS84"),
            best_solution = as.logical(best_solution)) %>%
     st_as_sf() %>%
     left_join(puvspr_wide,by=c("pu_id"="pu"))%>% 
     st_transform(proj) 
 
-bioregions <- st_read("tutorial/CF_landscape/bioregion_short.shp",stringsAsFactors = FALSE) %>% 
+bioregions <- st_read("maps_for_pubs/bioregion_short.shp",stringsAsFactors = FALSE) %>% 
     st_transform(proj) %>% 
     mutate(BIOREGI=paste0("BIORE_",BIOREGI)) %>% 
     filter(BIOREGI %in% spec$name) %>% 
@@ -80,10 +145,10 @@ p1 <- ggplot(plot_brg)+
           plot.title = element_text(size=10))+
     ggtitle("A) Representation Data")
 
-p2 <- ggplot(output)+
+p2 <- ggplot(output_connect)+
     geom_sf(data=AUS,fill="grey",colour="transparent")+
-    geom_sf(aes(fill=google_land_pu_R),colour="black",size=0.25)+
-    scale_fill_distiller("Selection\nFrequency",palette="OrRd",direction = 1,limits=c(0,0.007))+
+    geom_sf(aes(fill=between_cent_land_pu_mean),colour="black",size=0.25)+
+    scale_fill_distiller("Betweeness\nCentrality",palette="OrRd",direction = 1)+
     coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
              ylim=st_bbox(output_rotated)[c(2,4)],
              crs=proj_rotated,
@@ -92,7 +157,7 @@ p2 <- ggplot(output)+
           legend.direction = "vertical",
           legend.background = element_rect(fill="transparent"),
           plot.title = element_text(size=10))+
-    ggtitle("B) Google PageRank")
+    ggtitle("B) Connect. Feature")
 
 p3 <- ggplot(output)+
     geom_sf(data=AUS,fill="grey",colour="transparent")+
@@ -124,6 +189,6 @@ p4 <- ggplot(output_connect)+
     ggtitle("D) With Connectivity")
 
 
-ggsave("maps.png", grid.arrange(p1, p2, p3, p4, ncol=4),width=7.5,height=7.5,dpi=600)
+ggsave("maps_for_pubs/maps.png", grid.arrange(p1, p2, p3, p4, ncol=4),width=7.5,height=7.5,dpi=600)
 
 
