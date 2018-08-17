6e273b982552ce4559a774e0c94e0a3423e4b371
Remi Daigle <remi.daigle@dal.ca>
2018-08-17 15:59:46 -0300

    separated+multilayered the diff map

--- docs/maps_for_pubs/map_for_pub.R
+++ docs/maps_for_pubs/map_for_pub.R
@@ -2,6 +2,7 @@ library(sf)
 library(raster)
 library(gridExtra)
 library(tidyverse)
+library(magick)
 
 BIORE <- read.csv("maps_for_pubs/input/spec.dat") %>% 
     select(name) %>% 
@@ -88,8 +89,7 @@ puvspr_wide <- puvspr %>%
 # planning units with output
 output <- read.csv("maps_for_pubs/output/pu_no_connect.csv") %>%
     mutate(geometry=st_as_sfc(geometry,"+proj=longlat +datum=WGS84"),
-           best_solution = as.logical(best_solution),
-           select_prob=select_freq/20) %>%
+           best_solution = as.logical(best_solution)) %>%
     st_as_sf() %>%
     left_join(puvspr_wide,by=c("pu_id"="pu"))%>% 
     st_transform(proj)
@@ -99,8 +99,7 @@ output_rotated <- st_transform(output,proj_rotated)
 
 output_connect <- read.csv("maps_for_pubs/output/pu_connect.csv") %>%
     mutate(geometry=st_as_sfc(geometry,"+proj=longlat +datum=WGS84"),
-           best_solution = as.logical(best_solution),
-           select_prob=select_freq/20) %>%
+           best_solution = as.logical(best_solution)) %>%
     st_as_sf() %>%
     left_join(puvspr_wide,by=c("pu_id"="pu"))%>% 
     st_transform(proj) 
@@ -117,8 +116,8 @@ bioregions <- st_read("maps_for_pubs/bioregion_short.shp",stringsAsFactors = FAL
     st_as_sf(crs=proj)
 
 # bioregions[nrow(bioregions)+1,] <- st_as_sf(data.frame(BIOREGI="Planning Units",
-                                                       # SHORT_D="Planning Units",
-                                                       # geometry=st_combine(output)))
+# SHORT_D="Planning Units",
+# geometry=st_combine(output)))
 # bioregions$BIOREGI[nrow(bioregions)]="Planning Units"
 
 # get basemap outline of AUS
@@ -132,76 +131,151 @@ AUS <- getData(country="AUS",level=0) %>%
     st_transform(proj) %>% 
     st_intersection(st_buffer(bb,100000))
 
-bioregions$BIOREGI
-plot_lyrs <- c("Planning Units","BIORE_27","BIORE_3","BIORE_26")
-plot_brg <- bioregions %>% 
-    filter(BIOREGI %in% plot_lyrs)
+
+#### 3 panel #####
 
 p1 <- ggplot(output_connect)+
-    geom_sf(data=AUS,fill="grey",colour="transparent")+
-    geom_sf(aes(fill=between_cent_land_pu_mean),colour="black",size=0.25)+
-    scale_fill_distiller("Betweeness\nCentrality",palette="OrRd",direction = 1)+
+    geom_sf(data=AUS,fill="grey",colour="darkgrey")+
+    geom_sf(aes(fill=between_cent_land_pu_mean),colour="transparent",size=0.25)+
+    scale_fill_distiller("Betweeness\nCentrality",palette="Oranges",direction = 1)+
     coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
              ylim=st_bbox(output_rotated)[c(2,4)],
              crs=proj_rotated,
              expand = FALSE)+
+    theme_dark()+
     theme(legend.position = "bottom",
           legend.direction = "vertical",
           legend.background = element_rect(fill="transparent"),
-          plot.title = element_text(size=10))+
+          plot.title = element_text(size=10)) +
     ggtitle("A) Connect. Feature")
 
 p2 <- ggplot(output)+
-    geom_sf(data=AUS,fill="grey",colour="transparent")+
-    geom_sf(aes(fill=select_freq/20*100),colour="black",size=0.25)+
-    scale_fill_distiller("Selection\nFrequency (%)",palette="BuGn",direction = 1,limits=c(0,100))+
+    geom_sf(data=AUS,fill="grey",colour="darkgrey")+
+    geom_sf(aes(fill=select_freq),colour="transparent",size=0.25)+
+    scale_fill_distiller("Selection\nFrequency",palette="Purples",direction = 1,limits=c(0,100))+
     coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
              ylim=st_bbox(output_rotated)[c(2,4)],
              crs=proj_rotated,
              expand = FALSE)+
+    theme_dark()+
     theme(legend.position = "bottom",
           legend.direction = "vertical",
           legend.background = element_rect(fill="transparent"),
           plot.title = element_text(size=10))+
     ggtitle("B) Without Connectivity")
-          
+
 
 p3 <- ggplot(output_connect)+
-    geom_sf(data=AUS,fill="grey",colour="transparent")+
-    geom_sf(aes(fill=select_freq/20*100),colour="black",size=0.25)+
-    scale_fill_distiller("Selection\nFrequency (%)",palette="BuGn",direction = 1,limits=c(0,100))+
+    geom_sf(data=AUS,fill="grey",colour="darkgrey")+
+    geom_sf(aes(fill=select_freq),colour="transparent",size=0.25)+
+    scale_fill_distiller("Selection\nFrequency",palette="Oranges",direction = 1,limits=c(0,100))+
     coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
              ylim=st_bbox(output_rotated)[c(2,4)],
              crs=proj_rotated,
              expand = FALSE)+
+    theme_dark()+
     theme(legend.position = "bottom",
           legend.direction = "vertical",
           legend.background = element_rect(fill="transparent"),
           plot.title = element_text(size=10))+
     ggtitle("C) With Connectivity")
 
+
+ggsave("maps_for_pubs/maps.png", grid.arrange(p1, p2, p3, ncol=3),width=7.25,height=8,dpi=600)
+
+#### difference ####
 output_connect$diff <- output_connect$select_freq-output$select_freq
+output_connect$rep_select_freq <- output$select_freq
 
+oc_greater90 <- output_connect %>% 
+    filter(rep_select_freq>quantile(rep_select_freq,probs=0.9),
+           select_freq>quantile(select_freq,probs=0.9))
+oc_bottom5 <- output_connect %>% 
+    filter(rep_select_freq<5,
+           select_freq<5)
+oc_sfcon <- output_connect %>% 
+    filter(diff>5)
+oc_sfrep <- output_connect %>% 
+    filter(diff<(-5))
 
-p4 <-
-ggplot(output_connect)+
-    geom_sf(data=AUS,fill="grey",colour="transparent")+
-    geom_sf(aes(fill=diff/20*100),colour="black",size=0.25)+
-    scale_fill_distiller("Connectivity\nDifference",palette="BrBG",direction = 1,limits=c(-100,100))+
+base <- ggplot(oc_sfrep)+
+    geom_sf(data=AUS,fill="grey",colour="darkgrey")+
+    geom_sf(aes(fill=diff),colour="transparent")+
+    scale_fill_distiller("Decreases with\nConnectivity",palette=c("Purples"),limits=c(-100,-5))+
     coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
              ylim=st_bbox(output_rotated)[c(2,4)],
              crs=proj_rotated,
              expand = FALSE)+
-    theme(legend.position = "bottom",
+    theme_dark()+
+    theme(legend.position = c(1.625,0.2),
           legend.direction = "vertical",
+          plot.margin = margin(t=5.5,b=5.5,l=0,r=140,unit = "pt"),
+          legend.title = element_text(size=10))+
+    guides(fill = guide_legend(title.position = "right",
+                               reverse = TRUE))
+
+ggsave("maps_for_pubs/base.png",base,height=6,width=4,dpi=600)
+
+
+connect <- ggplot(oc_sfcon)+
+    geom_sf(aes(fill=diff),colour="transparent")+
+    scale_fill_distiller("Increases with\nConnectivity",palette=c("Oranges"),direction=1,limits=c(5,100))+
+    coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
+             ylim=st_bbox(output_rotated)[c(2,4)],
+             crs=proj_rotated,
+             expand = FALSE)+
+    theme_dark()+
+    theme(legend.position = c(1.59,0.8),
+          legend.direction = "vertical",
+          plot.margin = margin(t=5.5,b=5.5,l=0,r=140,unit = "pt"),
+          legend.title = element_text(size=10),
           legend.background = element_rect(fill="transparent"),
-          plot.title = element_text(size=10))+
-    ggtitle("D) Difference")
+          rect = element_rect(fill = "transparent"),
+          panel.grid = element_line(colour="transparent"),
+          panel.background = element_rect("transparent"),
+          axis.ticks = element_line("transparent"),
+          axis.text.x = element_text(colour="transparent"),
+          axis.text.y = element_text(colour="transparent"))+
+    guides(fill = guide_legend(title.position = "right",
+                               reverse = TRUE))
 
-ggsave("maps_for_pubs/maps.png", grid.arrange(p1, p2, p3, p4, ncol=4),width=7.5,height=7.5,dpi=600)
+ggsave("maps_for_pubs/connect.png",connect,bg="transparent",height=6,width=4,dpi=600)
 
 
+categorical <- ggplot()+
+    geom_sf(data=oc_greater90, aes(fill="A",colour="A"))+
+    geom_sf(data=oc_bottom5, aes(fill="B",colour="B"))+
+    scale_fill_manual("",values=c("#549b16","white"),labels=c("Always Irreplaceable","Rarely selected"))+
+    scale_colour_manual("",values=c("transparent","black"),labels=c("Always Irreplaceable","Rarely selected"))+
+    coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
+             ylim=st_bbox(output_rotated)[c(2,4)],
+             crs=proj_rotated,
+             expand = FALSE)+
+    theme_dark()+
+    theme(legend.position = c(1.65,0.5),
+          legend.direction = "vertical",
+          plot.margin = margin(t=5.5,b=5.5,l=0,r=140,unit = "pt"),
+          legend.text = element_text(size=10),
+          legend.background = element_rect(fill="transparent"),
+          rect = element_rect(fill = "transparent",colour="transparent"),
+          panel.grid = element_line(colour="transparent"),
+          panel.background = element_rect("transparent"),
+          plot.background = element_rect("transparent"),
+          axis.ticks = element_line("transparent"),
+          axis.text.x = element_text(colour="transparent"),
+          axis.text.y = element_text(colour="transparent"))+
+    guides(fill = guide_legend(title.position = "right"))
 
+ggsave("maps_for_pubs/categorical.png",categorical,bg="transparent",height=6,width=4,dpi=600)
+
+
+categorical <- image_read("maps_for_pubs/categorical.png")
+con <- image_read("maps_for_pubs/connect.png")
+base <- image_read("maps_for_pubs/base.png")
+composite <- image_composite(image_composite(base,con),categorical)
+image_write(composite,"maps_for_pubs/diff.png")
+
+#### bioregions ####
 
 bio <- ggplot()+
     geom_sf(data=AUS,fill="grey",colour="transparent")+
@@ -209,11 +283,12 @@ bio <- ggplot()+
     geom_sf(data=output_rotated,size=0.25,aes(fill="red",colour="red"))+
     facet_wrap(~ALT_LAB,nrow=2)+
     scale_fill_manual("",values=c("#1f78b4","transparent"),labels=c("Bioregion","Planning Units"))+
-    scale_colour_manual("",values=c("transparent","black"),labels=c("Bioregion","Planning Units"))+
+    scale_colour_manual("",values=c("transparent","lightgrey"),labels=c("Bioregion","Planning Units"))+
     coord_sf(xlim=st_bbox(output_rotated)[c(1,3)],
              ylim=st_bbox(output_rotated)[c(2,4)],
              crs=proj_rotated,
              expand = FALSE)+
+    theme_dark()+
     theme(legend.position = "bottom",
           legend.direction = "horizontal")
 ggsave("maps_for_pubs/bio_maps.png", bio,width=8.25,height=7.5,dpi=600)
